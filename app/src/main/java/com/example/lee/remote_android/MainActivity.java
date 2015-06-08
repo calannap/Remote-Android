5f55ce9e4c81c7009a2afb51c93b56328e0009ea
nivar <89gabriel@live.it>
2015-06-08 16:41:50 +0200

    prova

--- app/src/main/java/com/example/lee/remote_android/MainActivity.java
+++ app/src/main/java/com/example/lee/remote_android/MainActivity.java
@@ -3,24 +3,45 @@ package com.example.lee.remote_android;
 import android.app.ListActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends ActionBarActivity {
+
+    private Button button;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // codice "umano"
-        String [] s1 = {"Spegni device"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getListView().getContext(), android.R.layout.simple_expandable_list_item_1,s1);
-        getListView().setAdapter(adapter);
+       // String [] s1 = {"Spegni device"};
+       // ArrayAdapter<String> adapter = new ArrayAdapter<String>(getListView().getContext(), android.R.layout.simple_expandable_list_item_1,s1);
+       // getListView().setAdapter(adapter);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(mButtonStopListener);
 
     }
+    View.OnClickListener mButtonStopListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            try {
+                //funziona solo su quelli con il root
+                Runtime.getRuntime().exec(new String[]{ "su", "-c", "reboot -p" });
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                Log.e("onPause", e.getMessage());
+            }
 
+        }
+    };
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
