3d788a74b261ae81a4a75234043963ea2305ddb5
Lee <calannap@gmail.com>
2015-06-17 23:32:02 +0200

    dsada
    dsadasdas

--- app/src/main/java/com/example/lee/remote_android/HttpPostActivity.java
+++ app/src/main/java/com/example/lee/remote_android/HttpPostActivity.java
@@ -28,68 +28,67 @@ import java.util.ArrayList;
 
 public class HttpPostActivity extends Activity {
 
-        @Override
-        public  void  onCreate(Bundle savedInstanceState) {
-        super  .onCreate(savedInstanceState);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         final TextView textviewDatiRicevuti = (TextView) findViewById(R.id.datiRicevuti);
         Button buttonInviaDati = (Button) findViewById(R.id.buttonInviaDati);
-        buttonInviaDati.setOnClickListener(  new  View.OnClickListener() {
-        public  void  onClick(View view) {
-        //invio richiesta
-        textviewDatiRicevuti.setText(inviaDati());
-        }
+        buttonInviaDati.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                //invio richiesta
+                textviewDatiRicevuti.setText(inviaDati());
+            }
         });
-        }
-        public  String inviaDati(){
-        String result =   ""  ;
-        String stringaFinale =   ""  ;
-        ArrayList<NameValuePair> nameValuePairs =   new ArrayList <NameValuePair>();
-        nameValuePairs.add(  new BasicNameValuePair(  "idnomerichiesto"  ,  "1"  ));
-        InputStream is =   null  ;
+    }
+
+    public String inviaDati() {
+        String result = "";
+        String stringaFinale = "";
+        ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+        nameValuePairs.add(new BasicNameValuePair("idnomerichiesto", "1"));
+        InputStream is = null;
         //http post
-        try  {
-        HttpClient httpclient =   new DefaultHttpClient();
-        HttpPost httppost =   new  HttpPost(  "http://10.1.1.1:3306/miosito/connectDb.php"  );
-        httppost.setEntity(  new UrlEncodedFormEntity(nameValuePairs));
-        HttpResponse response = httpclient.execute(httppost);
-        HttpEntity entity = response.getEntity();
-        is = entity.getContent();
-        }  catch  (Exception e){
-        Log.e("TEST", "Errore nella connessione http " + e.toString());
+        try {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost("http://10.1.1.1:3306/miosito/connectDb.php");
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            HttpResponse response = httpclient.execute(httppost);
+            HttpEntity entity = response.getEntity();
+            is = entity.getContent();
+        } catch (Exception e) {
+            Log.e("TEST", "Errore nella connessione http " + e.toString());
         }
-        if  (is !=   null  ){
-        //converto la risposta in stringa
-        try  {
-        BufferedReader reader =   new  BufferedReader(  new InputStreamReader(is,  "iso-8859-1"  ),  8  );
-        StringBuilder sb =   new  StringBuilder();
-        String line =   null  ;
-        while  ((line = reader.readLine()) !=   null  ) {
-        sb.append(line +   "\n"  );
+        if (is != null) {
+            //converto la risposta in stringa
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8);
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line + "\n");
+                }
+                is.close();
+                result = sb.toString();
+            } catch (Exception e) {
+                Log.e("TEST", "Errore nel convertire il risultato " + e.toString());
+            }
+            //parsing dei dati arrivati in formato json
+            try {
+                JSONArray jArray = new JSONArray(result);
+                for (int i = 0; i < jArray.length();i++){
+                    JSONObject json_data = jArray.getJSONObject(i);
+                    Log.i("TEST", "id: " + json_data.getInt("id") +
+                                    ", cognome: " + json_data.getString("cognome") +
+                                    ", nascita: " + json_data.getInt("anno")
+                    );
+                    stringaFinale = json_data.getInt("id") + " " + json_data.getString("cognome") + " " + json_data.getInt("anno") + "\n\n";
+                }
+            } catch (JSONException e) {
+                Log.e("log_tag", "Error parsing data " + e.toString());
+            }
+        } else {  //is è null e non ho avuto risposta
         }
-        is.close();
-        result=sb.toString();
-        }  catch  (Exception e){
-        Log.e(  "TEST"  ,   "Errore nel convertire il risultato "  +e.toString());
-        }
-        //parsing dei dati arrivati in formato json
-        try  {
-        JSONArray jArray =   new  JSONArray(result);
-        for  (  int  i=  0  ;i&lt;jArray.length();i++){
-        JSONObject json_data = jArray.getJSONObject(i);
-        Log.i(  "TEST"  ,  "id: "  +json_data.getInt(  "id"  )+
-        ", cognome: "  +json_data.getString(  "cognome"  )+
-        ", nascita: "  +json_data.getInt(  "anno"  )
-        );
-        stringaFinale = json_data.getInt(  "id"  ) +   " "  + json_data.getString(  "cognome"  ) +   " "  + json_data.getInt(  "anno"  ) +   "\n\n"  ;
-        }
-        }
-        catch  (JSONException e){
-        Log.e(  "log_tag"  ,   "Error parsing data "  +e.toString());
-        }
-        }
-        else  {  //is è null e non ho avuto risposta
-        }
-        return  stringaFinale;
-        }
-        }
+        return stringaFinale;
+    }
+}
