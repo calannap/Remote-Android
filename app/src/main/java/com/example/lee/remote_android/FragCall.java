808b5b519310f44246db9274da6b367172f417cb
Lee <calannap@gmail.com>
2015-09-06 17:59:21 +0200

    ggwp

--- app/src/main/java/com/example/lee/remote_android/FragCall.java
+++ app/src/main/java/com/example/lee/remote_android/FragCall.java
@@ -20,7 +20,7 @@ public class FragCall extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_frag_rubrica, container, false);
+        View view = inflater.inflate(R.layout.fragment_frag_call, container, false);
 
         aereo=(Button) view.findViewById(R.id.button5);
 
